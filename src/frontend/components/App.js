335411734a0ea77591411e25efd1cc012f30471d
rajatrai30 <mailmerairajat@mail.com>
2023-10-15 21:31:51 +0530

    init

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,56 +1,64 @@
-import {
-  Link,
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import { useState } from 'react'
-import { ethers } from "ethers"
-import MusicNFTMarketplaceAbi from '../contractsData/MusicNFTMarketplace.json'
-import MusicNFTMarketplaceAddress from '../contractsData/MusicNFTMarketplace-address.json'
-import { Spinner, Navbar, Nav, Button, Container } from 'react-bootstrap'
-import logo from './logo.png'
-import Home from './Home.js'
-import MyTokens from './MyTokens.js'
-import MyResales from './MyResales.js'
-import './App.css';
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { ethers } from "ethers";
+import MusicNFTMarketplaceAbi from "../contractsData/MusicNFTMarketplace.json";
+import MusicNFTMarketplaceAddress from "../contractsData/MusicNFTMarketplace-address.json";
+import { Spinner, Navbar, Nav, Button, Container } from "react-bootstrap";
+// import logo from "./logo.png";
+import Home from "./Home.js";
+import MyTokens from "./MyTokens.js";
+import MyResales from "./MyResales.js";
+import "./App.css";
+import Welcome from "./Welcome";
 
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [account, setAccount] = useState(null)
-  const [contract, setContract] = useState({})
+  const [loading, setLoading] = useState(true);
+  const [account, setAccount] = useState(null);
+  const [contract, setContract] = useState({});
 
   const web3Handler = async () => {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    setAccount(accounts[0])
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccount(accounts[0]);
     // Get provider from Metamask
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
     // Get signer
-    const signer = provider.getSigner()
-    loadContract(signer)
-  }
+    const signer = provider.getSigner();
+    loadContract(signer);
+  };
   const loadContract = async (signer) => {
     // Get deployed copy of music nft marketplace contract
-    const contract = new ethers.Contract(MusicNFTMarketplaceAddress.address, MusicNFTMarketplaceAbi.abi, signer)
-    setContract(contract)
-    setLoading(false)
-  }
+    const contract = new ethers.Contract(
+      MusicNFTMarketplaceAddress.address,
+      MusicNFTMarketplaceAbi.abi,
+      signer
+    );
+    setContract(contract);
+    setLoading(false);
+  };
   return (
     <BrowserRouter>
       <div className="App">
         <>
           <Navbar expand="lg" bg="secondary" variant="dark">
             <Container>
-              <Navbar.Brand href="http://www.dappuniversity.com/bootcamp">
-                <img src={logo} width="40" height="40" className="" alt="" />
-                &nbsp; Music NFT player
+              <Navbar.Brand href="https://github.com/rajatrai30/DeciSound">
+                {/* <img src={logo} width="40" height="40" className="" alt="" /> */}
+                &nbsp; DeciSound
               </Navbar.Brand>
               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
               <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
-                  <Nav.Link as={Link} to="/">Home</Nav.Link>
-                  <Nav.Link as={Link} to="/my-tokens">My Tokens</Nav.Link>
-                  <Nav.Link as={Link} to="/my-resales">My Resales</Nav.Link>
+                  <Nav.Link as={Link} to="/">
+                    Home
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/my-tokens">
+                    My Tokens
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/my-resales">
+                    My Resales
+                  </Nav.Link>
                 </Nav>
                 <Nav>
                   {account ? (
@@ -58,14 +66,16 @@ function App() {
                       href={`https://etherscan.io/address/${account}`}
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="button nav-button btn-sm mx-4">
+                      className="button nav-button btn-sm mx-4"
+                    >
                       <Button variant="outline-light">
-                        {account.slice(0, 5) + '...' + account.slice(38, 42)}
+                        {account.slice(0, 5) + "..." + account.slice(38, 42)}
                       </Button>
-
                     </Nav.Link>
                   ) : (
-                    <Button onClick={web3Handler} variant="outline-light">Connect Wallet</Button>
+                    <Button onClick={web3Handler} variant="outline-light">
+                      Connect Wallet
+                    </Button>
                   )}
                 </Nav>
               </Navbar.Collapse>
@@ -74,27 +84,45 @@ function App() {
         </>
         <div>
           {loading ? (
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '80vh' }}>
-              <Spinner animation="border" style={{ display: 'flex' }} />
-              <p className='mx-3 my-0'>Awaiting Metamask Connection...</p>
-            </div>
+            <>
+              <div
+                style={{
+                  position: "fixed",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                }}
+              >
+                <Welcome />
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Spinner animation="border" style={{ display: "flex" }} />
+                  <p className="mx-3 my-0">Awaiting Metamask Connection...</p>
+                </div>
+              </div>
+            </>
           ) : (
             <Routes>
-              <Route path="/" element={
-                <Home contract={contract} />
-              } />
-              <Route path="/my-tokens" element={
-                <MyTokens contract={contract} />
-              } />
-              <Route path="/my-resales" element={
-                <MyResales contract={contract} account={account} />
-              } />
+              <Route path="/" element={<Home contract={contract} />} />
+              <Route
+                path="/my-tokens"
+                element={<MyTokens contract={contract} />}
+              />
+              <Route
+                path="/my-resales"
+                element={<MyResales contract={contract} account={account} />}
+              />
             </Routes>
           )}
         </div>
       </div>
     </BrowserRouter>
-
   );
 }
 
---
+++ src/frontend/components/Welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div>
+      <h2>Welcome To DeciSound!!</h2>
+      <h5>A Decentralized Music App</h5>
+    </div>
+  );
+};
+
+export default Welcome;
